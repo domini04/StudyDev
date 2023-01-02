bded988734f357947d916efa60876f3079852ea5
Shin <sinyubi04@gmail.com>
2023-01-02 15:35:47 +0900

    Trees update

--- Data-Structures/11. Trees/TreeNode.py
+++ Data-Structures/11. Trees/TreeNode.py
@@ -1,8 +1,45 @@
-class TreeNode
-    def __init__(self, val):
-        print("TreeNode.__init__({0})".format(val))
-        self.val = val
-        self.left = None
-        self.right = None
+# I. Basic Tree
+class TreeNode :
+  def __init__(self, value) :
+    self.value = value
+    self.children = []
+    self.parent = None
 
-Seed = TreeNode("Shin")
+  def add_child(self, child_node) : 
+    child_node.parent = self
+    self.children.append(child_node)
+
+  def remove_child(self, child_node) :
+    self.children = [child for child in self.children if child is not child_node]
+
+  def traverse(self) :
+    nodes_to_visit = [self]
+    while len(nodes_to_visit) > 0 :
+      current_node = nodes_to_visit.pop()
+      print(current_node.value)
+      nodes_to_visit += current_node.children
+
+
+# II. Binary Tree
+# class TreeNode :
+#     def __init__(self, val):
+#         print("TreeNode.__init__({0})".format(val))
+#         self.val = val
+#         self.left = None
+#         self.right = None
+
+#     def add_child (self, child_node):
+#         print("TreeNode.add_child({0})".format(child_node.val))
+#         #자신보다 작으면 왼쪽, 크면 오른쪽에 추가
+#         #만약 자식이 있으면 자식에게 위임
+#         if child_node.val < self.val: 
+#             if self.left: # if left node exists
+#                 self.left.add_child(child_node)
+#             else:
+#                 self.left = child_node
+#         else:
+#             if self.right:
+#                 self.right.add_child(child_node)
+#             else:
+#                 self.right = child_node
+# Seed = TreeNode("Shin")
