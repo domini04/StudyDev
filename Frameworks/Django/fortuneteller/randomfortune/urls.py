1e358e53ec50006ba847fd08b5f14e0b28dbba83
Shin <sinyubi04@gmail.com>
2023-02-10 20:12:43 +0900

    Delete Django

--- Frameworks/Django/fortuneteller/randomfortune/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.fortune, name='fortune'), #path() : function that takes 3 arguments: route, view, and name
-]
